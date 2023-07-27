cee3e6da5c35245603693bf650abc7bc2cbd8e0a
General-Wau <elven@gmx.com>
2023-07-27 12:35:10 +0200

    Added ocean base for intial ocean levels

--- app/src/main/java/com/necromyd/oceandepths/MainActivity.kt
+++ app/src/main/java/com/necromyd/oceandepths/MainActivity.kt
@@ -3,13 +3,26 @@ package com.necromyd.oceandepths
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.times
 import com.necromyd.oceandepths.ui.theme.OceanDepthsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +30,130 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             OceanDepthsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                OceanDepthApp()
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun OceanDepthApp() {
+    Column {
+        Box(
+            modifier = Modifier.fillMaxWidth()
+                .height(150.dp)
+                .background(Color.Yellow)
+        ) {
+            // Content of the first box (e.g., landmarks, sea creatures, etc.)
+        }
+        Box(
+            modifier = Modifier.fillMaxWidth()
+                .height(300.dp)
+                .background(Color.Blue)
+        ) {
+            // Content of the second box (e.g., landmarks, sea creatures, etc.)
+        }
+        Box(
+            modifier = Modifier.fillMaxWidth()
+                .height(100.dp)
+                .background(Color.Black)
+        ) {
+            // Content of the second box (e.g., landmarks, sea creatures, etc.)
+        }
+    }
+}
+
+
+@Composable
+private fun getSecondBoxHeight(): Dp {
+    val configuration = LocalConfiguration.current
+    val screenHeight: Dp = with(LocalDensity.current) {
+        configuration.screenHeightDp.dp
+    }
+
+    // Define the heights for each depth level
+    val heights = listOf(100.dp, 200.dp, 500.dp)
+    val depths = listOf(1000.dp, 3000.dp, 11000.dp)
+
+    // Find the index of the closest depth level based on the current screenHeight
+    val closestDepthIndex = depths.indexOfFirst { it > screenHeight }
+        .takeIf { it != -1 } ?: depths.lastIndex
+
+    // Calculate the second box height based on the closest depth level and the corresponding height
+    return (screenHeight.value / depths[closestDepthIndex].value) * heights[closestDepthIndex]
+}
+
+@Composable
+fun TitleScreenContent() {
+    var showContent by remember { mutableStateOf(true) }
+    val context = LocalContext.current
+    val density = LocalDensity.current
+    val customButtonColor = MaterialTheme.colors.primaryVariant.copy(alpha = 0.8f)
+
+    if (showContent) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center,
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(
+                    text = "Ocean Depths",
+                    style = MaterialTheme.typography.h2,
+                    color = MaterialTheme.colors.primary,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
+                )
+                Spacer(modifier = Modifier.height(110.dp))
+                Button(
+                    modifier = Modifier
+                        .width(200.dp)
+                        .height(50.dp),
+                    onClick = {
+                        showContent = !showContent
+                    },
+                    shape = RoundedCornerShape(20.dp),
+                    elevation = ButtonDefaults.elevation(
+                        defaultElevation = 10.dp,
+                        pressedElevation = 15.dp,
+                        disabledElevation = 0.dp
+                    ),
+                    colors = ButtonDefaults.buttonColors(customButtonColor)
+                ) {
+                    Text(text = "Begin", fontSize = 25.sp)
+                }
+                // Add any other UI elements or text at the bottom as needed.
+                Spacer(modifier = Modifier.height(120.dp))
+            }
+        }
+
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.BottomEnd
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(7.dp),
+                horizontalArrangement = Arrangement.End
+            ) {
+                Text(
+                    text = "App by Necromyd , 2023",
+                    fontSize = 15.sp,
+                    color = Color.Gray
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     OceanDepthsTheme {
-        Greeting("Android")
     }
 }
